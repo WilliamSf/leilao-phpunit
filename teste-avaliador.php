6ecc1c5c4935c80c6a6895041de69cf98752bccf
William Silva <william.faria@rarolabs.com.br>
2022-03-17 16:24:41 -0300

    instala phpunit

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -7,6 +7,15 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+/**
+ * Padrão Arrange-Act-Assert
+ * @link http://wiki.c2.com/?ArrangeActAssert
+ * 
+ * Padrão Given-When-Then
+ * @link https://martinfowler.com/bliki/GivenWhenThen.html
+*/
+
+// Arrange - Given / Preparamos o cenário do teste
 $leiao = new Leilao('Fiat 147 0KM');
 
 $maria = new Usuario('Maria');
@@ -15,9 +24,16 @@ $joao  = new Usuario('João');
 $leiao->recebeLance(new Lance($joao, 2000));
 $leiao->recebeLance(new Lance($maria, 2500));
 
+// Act - When / Executamos o código a ser testado
 $leiloeiro =  new Avaliador();
 $leiloeiro->avalia($leiao);
 
+// Assert - Then / Verificamos se a saída é a esperada
+$valorEsperado = 2500;
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor;
+if ($valorEsperado == $maiorValor) {
+    echo 'Teste ok';
+} else {
+    echo 'Resultado não esperado';
+}
